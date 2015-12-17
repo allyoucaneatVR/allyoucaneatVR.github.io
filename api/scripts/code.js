b8712a4556bfc477d544602740eccbd5ee5ded76
Phelicks <FeJa-92@web.de>
2015-12-17 12:36:09 +0100

    updated api/front page

--- api/scripts/code.js
+++ api/scripts/code.js
@@ -2,16 +2,17 @@
 
 var lastElement = null;
 function collapse(element){
-    if(lastElement && lastElement !== element && element.parentNode != lastElement){
-        lastElement.style.display = "none";
-    }
-    
+//    if(lastElement && lastElement !== element && element.parentNode != lastElement){
+//        lastElement.style.display = "none";
+//    }
+    console.log(element.id);
     if(element.style.display === "none"){
         element.style.display = "block";
     }
     else{
         element.style.display = "none";
     }
+    event.stopPropagation(); 
     
-    lastElement = element;
+//    lastElement = element;
 }
